1d16a86310d4fc0c711ceace78538414cfa763e7
Duncan McGreggor <oubiwann@lorien>
2008-06-21 01:26:46 -0500

    Added we resources to the .tac file for rudimentary twisted admin web ui.

--- bin/pydir.tac
+++ bin/pydir.tac
@@ -1,25 +1,31 @@
 import resource
 
+from twisted.web import server
 from twisted.application import service
 from twisted.application import internet
 from twisted.application import strports
 
 from pydirector import main
+from pydirector.web import admin
 from pydirector.manager import checkBadHosts
 
 resource.setrlimit(resource.RLIMIT_NOFILE, (1024, 1024))
 
 application = service.Application('PyDirector')
+services = service.IServiceCollection(application)
 
 # set up the director
 configFile = './config.xml'
 director = main.Director(configFile)
 
 # set up the web server
-admin = None
+site = server.Site(admin.AdminServer(director))
+admin = internet.TCPServer(director.conf.admin.listen[1], site)
+admin.setServiceParent(services)
 
 # set up the manager timer service
+# XXX this interval needs to be in the configuration
 checkInterval = 120
 checker = internet.TimerService(checkInterval, checkBadHosts, director)
-checker.setServiceParent(application)
+checker.setServiceParent(services)
 
--- pydirector/web/admin.py
+++ pydirector/web/admin.py
@@ -1,1 +1,150 @@
+import time
+import urllib
+import socket
 
+from twisted.web import resource
+
+from pydirector import Version
+from pydirector.web import template
+
+class BasePage(resource.Resource):
+    """
+
+    """
+    def __init__(self, parent):
+        resource.Resource.__init__(self)
+        self.parent = parent
+
+    def getHeader(self, refreshURL=''):
+        """
+
+        """
+        refresh = ''
+        if refreshURL:
+            # XXX add an admin configuration option for setting the refresh
+            # rate
+            refreshRate = 30
+            refresh = template.refresh % (refreshRate, refreshURL)
+        return template.header % (
+            refresh, self.parent.serverVersion, socket.gethostname())
+
+    def getBody(self):
+        """
+        Subclasses must override this.
+        """
+        raise NotImplemented
+
+    def getFooter(self, message=''):
+        """
+
+        """
+        # XXX put the project URL in the admin config
+        projectURL ='http://pythondirector.sf.net'
+        if message:
+            message = template.message % urllib.unquote(message)
+        return template.footer % (projectURL, message)
+
+
+    def getPage(self):
+        """
+
+        """
+        return self.getHeader() + self.getBody() + self.getFooter()
+
+    def render_GET(self, request):
+        """
+
+        """
+        return self.getPage()
+
+class RunningPage(BasePage):
+    """
+
+    """
+    def getPage(self):
+        """
+
+        """
+        refresh = False
+        verbose = False
+        resultMessage = ''
+        content = self.getHeader(refreshURL='/running?refresh=1&ignore=%s' % time.time())
+        if refresh:
+            stopStart = template.stopRefresh % time.time()
+        else:
+            stopStart = template.startRefresh % time.time()
+        content += template.refreshButtons % (
+            time.ctime(time.time()), time.time(), stopStart)
+        for service in self.parent.director.conf.getServices():
+            content += template.serviceName % service.name
+            for l in service.listen:
+                content += template.listeningService % l
+            eg = service.getEnabledGroup()
+            groups = service.getGroups()
+            for group in groups:
+                sch = self.parent.director.getScheduler(service.name, group.name)
+                stats = sch.getStats(verbose=verbose)
+                hdict = sch.getHostNames()
+                if group is eg:
+                    klass = 'enabled'
+                    desc = template.groupDescEnabled
+                else:
+                    klass = 'inactive'
+                    desc = template.groupDescDisabled % (
+                        service.name, group.name)
+                content += template.groupName % (klass, group.name)
+                content += desc
+                content += template.groupHeaderForm % (
+                    service.name, group.name, klass)
+                counts = stats['open']
+                totals = stats['totals']
+                k = counts.keys()
+                k.sort()
+                for h in k:
+                    if counts.has_key(h):
+                        oc = counts[h]
+                    else:
+                        oc = '--'
+                    if totals.has_key(h):
+                        tc = totals[h]
+                    else:
+                        tc = '--'
+                    content += template.hostInfo % (
+                        klass, hdict[h], h, oc, tc, urllib.quote(service.name),
+                        urllib.quote(group.name), urllib.quote(h))
+                bad = stats['bad']
+                if bad:
+                    content += template.badHostGroup % klass
+                for k in bad.keys():
+                    host = '%s:%s' % k
+                    when, what = bad[k]
+                    content += template.badHostInfo % (
+                        klass, hdict[host], host, what)
+            content += template.serviceClose
+        content += self.getFooter(resultMessage)
+        return str(content)
+
+class AdminServer(resource.Resource):
+    """
+
+    """
+    def __init__(self, director):
+        resource.Resource.__init__(self)
+        self.director = director
+        self.config = director.conf.admin
+        self.starttime = time.time()
+        self.serverVersion = "pythondirector/%s"%Version
+
+    def getChild(self, name, request):
+        """
+
+        """
+        if name == 'running':
+            return RunningPage(self)
+        return resource.Resource.getChild(self, name, request)
+
+    def render_GET(self, request):
+        """
+
+        """
+        return "base"
