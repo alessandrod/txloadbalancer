be6797d2d6bfb6df0beff6a0b9b31f240f719203
Duncan McGreggor <oubiwann@lorien>
2008-06-21 17:05:35 -0500

    Incremented version number (for next release).

--- txlb/__init__.py
+++ txlb/__init__.py
@@ -1,12 +1,3 @@
-#
-# Copyright (c) 2002-2004 ekit.com Inc (http://www.ekit-inc.com)
-# and Anthony Baxter <anthony@interlink.com.au>
-#
-# $Id: __init__.py,v 1.13 2004/12/14 13:31:39 anthonybaxter Exp $
-#
-
-# Package init file.
-
 name = 'txLoadBalancer'
 shortName = 'txLB'
-version = '1.0.0'
+version = '1.2.0'
