d73fd1742bcb64e6c3b16646c972139ea8291a45
Duncan McGreggor <oubiwann@gondor>
2008-06-22 03:37:50 -0500

    2008.06.22 oubiwann
    * Added a script for getting stress test stats using the siege tool.
    * Added some initial stress test data.

---
+++ admin/runSiege.sh
@@ -0,0 +1,39 @@
+# This script will generate a files with rows of data having the following
+# columns, in order:
+#
+#  1) Sample Count
+#  2) Attempted Concurrency
+#  3) Elapsed Time
+#  4) Transaction Rate
+#  5) Throughput
+#  6) Concurrency
+#  7) Successful Transactions
+#  8) Failed Transactions
+#  9) CPU Count
+#  10) Twisted Daemon Count
+#  11) Is Loadbalanced?
+#  12) Balanced Host Count
+#
+LOG="data/stats.csv"
+URL="http://lorien:8080/index.html"
+LB="yes"
+LB_HOSTS=1
+WEB_SERVERS=4
+TOTAL_CPUS=2
+TIME=10s
+REST=5
+SAMPLES=10
+RANGE="1 2 5 10 20 30 40 50 60 70 80 90 100"
+for CONCUR in $RANGE
+    do
+    for ((SAMPLE=1; SAMPLE <= $SAMPLES; SAMPLE++))
+        do
+        DATA=`siege -c $CONCUR -b -t $TIME $URL 2>&1 | \
+            egrep 'Successful|Concurrency:|rate:|Failed|Elapsed|Throughput:' | \
+            awk -F: '{print $2}' | \
+            awk '{print $1 ","}'`
+        OUT="$SAMPLE, $CONCUR, $DATA $TOTAL_CPUS, $WEB_SERVERS, $LB, $LB_HOSTS"
+        echo $OUT >> $LOG
+        sleep $REST
+        done
+    done
