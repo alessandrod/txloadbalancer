908e67648f86ca902c1b165a20973f0116b247e1
oubiwann <oubiwann@fangorn>
2008-06-22 20:19:57 -0600

    * Updated siege runner admin script.

--- admin/startWebServers.sh
+++ admin/startWebServers.sh
@@ -1,13 +1,15 @@
 MAX=$1
 #DOC_ROOT=/Users/oubiwann/Sites/
-DOC_ROOT=/home/oubiwann/public_html
-FILE_PREFIX=twistd
+DOC_ROOT=/home/oubiwann/public_html/
+FILE_PREFIX=/tmp/twistd
 STARTING_PORT=7001
 cd /tmp
 for ((NUM=$STARTING_PORT; NUM <= $MAX; NUM++))
     do
+    echo "Removing old pid and log files..."
     rm ${FILE_PREFIX}${NUM}.pid
     rm ${FILE_PREFIX}${NUM}.log
+    echo "Starting up twistd on $NUM..."
     twistd --pidfile=${FILE_PREFIX}${NUM}.pid web \
         --port=${NUM} \
         --path=$DOC_ROOT \
